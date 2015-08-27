7ed470bfe9ce3a57971021f11bcf5a64d419fb57
Lyeec <hucxarch@gmail.com>
2015-08-27 19:37:28 +0800

    11单元练习题3完成

---
+++ Unit-11/Third/Third/Third.h
@@ -0,0 +1,33 @@
+
+// Third.h : Third 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CThirdApp:
+// 有关此类的实现，请参阅 Third.cpp
+//
+
+class CThirdApp : public CWinApp
+{
+public:
+	CThirdApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CThirdApp theApp;
