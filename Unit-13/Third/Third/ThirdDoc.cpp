5fabcbb374d5a8e0a67c4bf7f5c9ee3af38533f5
Lyeec <hucxarch@gmail.com>
2015-08-28 13:48:54 +0800

    13单元练习题3初始化

---
+++ Unit-13/Third/Third/ThirdDoc.cpp
@@ -0,0 +1,137 @@
+
+// ThirdDoc.cpp : CThirdDoc 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Third.h"
+#endif
+
+#include "ThirdDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CThirdDoc
+
+IMPLEMENT_DYNCREATE(CThirdDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CThirdDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CThirdDoc 构造/析构
+
+CThirdDoc::CThirdDoc()
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CThirdDoc::~CThirdDoc()
+{
+}
+
+BOOL CThirdDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CThirdDoc 序列化
+
+void CThirdDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CThirdDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CThirdDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CThirdDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = NULL;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != NULL)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CThirdDoc 诊断
+
+#ifdef _DEBUG
+void CThirdDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CThirdDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CThirdDoc 命令
---
+++ Unit-13/Third/Third/ThirdView.cpp
@@ -0,0 +1,104 @@
+
+// ThirdView.cpp : CThirdView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Third.h"
+#endif
+
+#include "ThirdDoc.h"
+#include "ThirdView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CThirdView
+
+IMPLEMENT_DYNCREATE(CThirdView, CView)
+
+BEGIN_MESSAGE_MAP(CThirdView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CThirdView 构造/析构
+
+CThirdView::CThirdView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CThirdView::~CThirdView()
+{
+}
+
+BOOL CThirdView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CThirdView 绘制
+
+void CThirdView::OnDraw(CDC* /*pDC*/)
+{
+	CThirdDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CThirdView 打印
+
+BOOL CThirdView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CThirdView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CThirdView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CThirdView 诊断
+
+#ifdef _DEBUG
+void CThirdView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CThirdView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CThirdDoc* CThirdView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CThirdDoc)));
+	return (CThirdDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CThirdView 消息处理程序
